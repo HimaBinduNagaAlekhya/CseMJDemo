9180d2486c172b6e2e4bb77b643a4cfca1703aca
HI <HI@DESKTOP-TDEOMCR>
2022-11-11 09:00:57 -0800

    This is my first Jenkins

---
+++ src/main/java/Jenkins/CseMJDemo/App.java
@@ -0,0 +1,13 @@
+package Jenkins.CseMJDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Jenkins" );
+    }
+}
